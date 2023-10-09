6389011ab1f6bb2ea7095545a2d0ef6f19ae5d81
sofiedwardsv <86111629+sofiedwardsv@users.noreply.github.com>
2023-10-08 23:24:51 -0300

    Update map.tsx

--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom'
 import './map.scss'
+
 
 
 const Map = () => {
@@ -9,7 +11,8 @@ const Map = () => {
       <h1>Interactive Map</h1>
       <div style={{ width: '100%', height: '100%', background: '#c1b8a4' }}>
       <iframe className='pantalla' title="Ypacarai Lake" src="//www.arcgis.com/apps/Embed/index.html?webmap=60f19bb8db264002934c5a07a176d93a&extent=-57.5499,-25.4287,-57.1067,-25.2012&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"></iframe>
-    </div>
+   <Link to="https://drive.google.com/drive/folders/1dnTVn99TXsxkLGYEfRAwljoQoPz0k67W?usp=share_link" target="_blank"style={{textAlign: 'left', fontSize: '20px', color: "white"}}>Find our products made from Landsat 8 data here</Link>
+      </div>
     </div>
   )
 }
